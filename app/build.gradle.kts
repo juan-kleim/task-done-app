f82d2a184300062bc145ba7c96ca7ddfddae7745
Juan Pacheco Kleim <pachkleim1918@gmail.com>
2025-02-18 16:14:04 -0300

    idk

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,7 +10,6 @@ android {
     defaultConfig {
         applicationId = "com.example.apptaskdone"
         minSdk = 29
-        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
